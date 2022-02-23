9ded42cb32eb26cddc175a66445cc634caf65975
Anthony <a.airaud.dev@gmail.com>
2022-02-23 23:17:49 +0100

    js translation En, conditional color for car info

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 let infoperso = {
     template: `
-    <section id="infoPerso" class="conteneur flex">
+    <section v-if="info" id="infoPerso" class="conteneur flex">
     <div>
-    <h2>{{info.nom}} {{info.prenom}}</h2>
-    <p v-if="info.adresse">Adresse : {{info.adresse}} </p>
-    <p v-if="age">Age : {{age}} ans</p>
-    <p v-if="vehicule">Voiture : {{this.vehicule}} </p>
+    <h2>{{info.lastName}} {{info.firstName}}</h2>
+    <p>{{info.adress}} </p>
+    <p>{{info.email}}</p>
+    <p>{{info.phone}}</p>
+    <p>{{age}} ans</p>
+    <p v-if="info.car" style="color:green">Voiture : {{this.vehicle}} </p>
+     <p v-else="info.car" style="color:red">Voiture : {{this.vehicle}} </p>
     </div>
     <div>
-    <img :src="trombi" alt="photo de profil"/>
+    <img v-if="trombi" :src="trombi" alt="photo de profil"/>
     </div>
 </section>
     `,
     props: ['info', 'trombi'],
     computed: {
-        vehicule: function () {
-            if (this.info.car != undefined)
+        vehicle: function () {
+            if (this.info)
                 return (this.info.car) ? 'oui' : 'non';
         },
         age: function () {
-            if (this.info.ddn) {
-                return Math.floor((new Date().getTime() - Date.parse(this.info.ddn)) / (1000 * 60 * 60 * 24 * 365.25));
+            if (this.info) {
+                return Math.floor((new Date().getTime() - Date.parse(this.info.birthDate)) / (1000 * 60 * 60 * 24 * 365.25));
             }
         }
     },
@@ -28,26 +31,26 @@ let infoperso = {
 
 let formation = {
     template: `
-    <section id="formation" class="conteneur">
+    <section v-if="schools" id="formation" class="conteneur">
     <h2>Formations</h2>
-        <div class="formDetail" v-for="formation of formations">
-            <h3>{{formation.nom}}</h3>
-            <span>Du {{formation.dateDebut}} au {{formation.dateFin}}</span>
-            <p v-html="formation.detail"></p>
+        <div class="formDetail" v-for="school of schools">
+            <h3>{{school.nom}}</h3>
+            <span>Du {{school.start}} au {{school.end}}</span>
+            <p v-html="school.detail"></p>
         </div>
     </section>
     `,
-    props: ['formations'],
+    props: ['schools'],
 };
 
 let experiencepro = {
     template: `
-    <section id="experience" class="conteneur">
+    <section v-if="experiences" id="experience" class="conteneur">
     <h2>Expériences professionnelles</h2>
-        <div class="exp" v-for="exp of experiences">
-            <h3>{{exp.fonction}}</h3>
-            <span>Du {{exp.dateDebut}} au {{exp.dateFin}}</span>
-            <p>{{exp.detail}}</p>
+        <div class="exp" v-for="experience of experiences">
+            <h3>{{experience.fonction}}</h3>
+            <span>Du {{experience.start}} au {{experience.end}}</span>
+            <p v-html="experience.detail"></p>
         </div>
     </section>
     `,
@@ -56,7 +59,7 @@ let experiencepro = {
 
 let competences = {
     template: `
-        <section id="competences" class="conteneur">
+        <section v-if="competences" id="competences" class="conteneur">
         <h2>Compétences</h2>
             <ul>
                 <li v-for="competence of competences">{{competence}}</li>
@@ -69,28 +72,28 @@ let competences = {
 let vm = new Vue({
     el: '#app',
     data: {
-        nom: null,
-        prenom: null,
-        dateNaissance: null,
-        adresse: null,
+        lastName: null,
+        firstName: null,
+        birthDate: null,
+        adress: null,
         phone: null,
         mail: null,
         url: null,
         car: null,
         intitule: null,
-        formDateDebut: null,
-        formDateFin: null,
+        formDateStart: null,
+        formDateEnd: null,
         formDetail: null,
-        formations: [],
+        schools: [],
         fonction: null,
-        fctDateDebut: null,
-        fctDateFin: null,
+        expDateStart: null,
+        expDateEnd: null,
         expDetail: null,
-        experiences: [],
-        comp: null,
-        competences: [],
-        infoPerso: [],
-        trombi: '',
+        history: [],
+        skill: null,
+        skills: [],
+        infoPerso: null,
+        trombi: null,
     },
     created: function () {
         let data = JSON.parse(localStorage.getItem('cv'));
@@ -100,40 +103,49 @@ let vm = new Vue({
     },
     computed: {
         age: function () {
-            if (this.dateNaissance) {
-                return Math.floor((new Date().getTime() - Date.parse(this.dateNaissance)) / (1000 * 60 * 60 * 24 * 365.25));
+            if (this.birthDate) {
+                return Math.floor((new Date().getTime() - Date.parse(this.birthDate)) / (1000 * 60 * 60 * 24 * 365.25));
             }
         }
     },
     methods: {
         infoAdd: function () {
-            if (this.nom != null && this.prenom != null && this.dateNaissance != null && this.adresse != null && this.phone != null && this.mail != null) {
-                let date = this.dateNaissance.toString();
-                this.infoPerso = { ddn: date, nom: this.nom, prenom: this.prenom, adresse: this.adresse, car: this.car };
-            };
+            if (this.lastName != null && this.firstName != null && this.birthDate != null && this.adress != null && this.phone != null && this.mail != null && this.car != null) {
+                let date = this.birthDate.toString();
+                this.infoPerso = { birthDate: date, lastName: this.lastName, firstName: this.firstName, adress: this.adress, car: this.car, email: this.mail, phone: this.phone };
+            } else alert('Veuillez completer le formulaire !');
         },
         formAdd: function () {
-            let start = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.formDateDebut)).toString();
-            let end = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.formDateFin)).toString();
+            let start = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.formDateStart)).toString();
+            let end = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.formDateEnd)).toString();
             let detail = this.formDetail.replaceAll("\n", "<br/>");
-            this.formations.push({ nom: this.intitule, dateDebut: start, dateFin: end, detail: detail });
+            this.schools.push({ nom: this.intitule, start: start, end: end, detail: detail });
             this.intitule = null;
-            this.formDateDebut = null;
-            this.formDateFin = null;
+            this.formDateStart = null;
+            this.formDateEnd = null;
             this.formDetail = null;
         },
         expAdd: function () {
-            let start = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.fctDateDebut)).toString();
-            let end = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.fctDateFin)).toString();
-            this.experiences.push({ fonction: this.fonction, dateDebut: start, dateFin: end, detail: this.expDetail });
-            this.fctDateDebut = null;
-            this.fctDateFin = null;
+            let start = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.expDateStart)).toString();
+            let end = Intl.DateTimeFormat("fr-FR").format(Date.parse(this.expDateEnd)).toString();
+            let detail = this.expDetail.replaceAll("\n", "<br/>");
+            this.history.push({ fonction: this.fonction, start: start, end: end, detail: detail });
+            this.expDateStart = null;
+            this.expDateEnd = null;
             this.fonction = null;
             this.expDetail = null;
         },
+        clear: function () {
+            this.lastName = null;
+            this.firstName = null;
+            this.birthDate = null;
+            this.adress = null;
+            this.phone = null;
+            this.mail = null;
+            this.car = null;
+        },
         compAdd: function () {
-            this.competences.push(this.comp);
-            this.comp = null;
+            this.skills.push(this.skill);
         },
         addPicture: function () {
             this.trombi = this.url
@@ -144,6 +156,10 @@ let vm = new Vue({
         clearLocalStorage: function () {
             if (confirm("Confirmez vous la suppression ?"))
                 localStorage.removeItem('cv');
+        },
+        testfct: function () {
+            for (comp of this.skills) console.log(comp)
+            console.log(this.skills[0])
         }
     },
     components: {
